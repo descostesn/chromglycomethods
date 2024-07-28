f3f606ab8ccdb3cf51d0d30513c8b55ca0703b99
descoste <nicolas.descostes@embl.it>
2024-07-28 16:50:45 +0200

    restrict parameters

--- figure1/C/peaksonproms.R
+++ figure1/C/peaksonproms.R
@@ -12,7 +12,6 @@ library("GenomicFeatures")
 library("ChIPpeakAnno")
 library("RColorBrewer")
 library("ggplot2")
-library("Rargs")
 library("biomaRt")
 library("reshape2")
 
@@ -22,71 +21,8 @@ library("reshape2")
 # PARAMETERS
 ################
 
-paramsDefinition <- list()
 
-paramsDefinition[["--queryFileVec"]] <- list(variableName="queryFileVec", numeric=F, mandatory=T, description="Path to the GFF file containing the peaks.")
-paramsDefinition[["--repeatFilesVec"]] <- list(variableName="repeatFilesVec", numeric=F, mandatory=T, description="Vector containg the GFF paths of the repeats.")
-paramsDefinition[["--repeatsNameVec"]] <- list(variableName="repeatsNameVec", numeric=F, mandatory=T, description="Vector containing the corresponding names of the repeats.")
-paramsDefinition[["--pieTitleVec"]] <- list(variableName="pieTitleVec", numeric=F, mandatory=T, description="Title to display for the peaks on the piechart.")
-paramsDefinition[["--outputFolder"]] <- list(variableName="outputFolder", numeric=F, mandatory=T, description="Path to the output folder.")
-paramsDefinition[["--enhancerspath"]] <- list(variableName="enhancerspath", numeric=F, mandatory=T, description="Path to the file containing the enhancer coordinates. GFF format.")
-## Optional
-paramsDefinition[["--species"]] <- list(variableName="species", numeric=F, mandatory=F, description="Should be mouse or human.", default = "mouse")
-
-queryFileVec <- 
-c(paste("/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG6hGlcNAc1_lane1sample15_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG6hGlcNAc2_lane1sample16_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG6hGlcNAc3_lane1sample17_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCHGGlcNAc1_lane1sample12_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCHGGlcNAc2_lane1sample13_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCHGGlcNAc3_lane1sample14_peaks_narrowPeak.gff", sep = " "),
-
-paste("/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG48hGlcNAc1_lane1sample18_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG48hGlcNAc2_lane1sample19_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG48hGlcNAc3_lane1sample20_peaks_narrowPeak.gff", sep = " "),
-
-paste("/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG1wGlcNAc1_lane1sample21_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG1wGlcNAc2_lane1sample22_peaks_narrowPeak.gff",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCLG1wGlcNAc3_lane1sample23_peaks_narrowPeak.gff", sep = " "))
-
-repeatFilesVec <- paste("/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/LINE.gff", 
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/LTR.gff",
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/SINE.gff", 
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/Satellite.gff",
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/DNA.gff",
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/Simple_repeat.gff",
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/RNA.gff",
-		"/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/repeatmasker/classes/gff/Low_complexity.gff", sep=" ")
-
-repeatsNameVec <- paste("LINE", "LTR", "SINE", "Satellite", "DNA", "Simple_repeat", "RNA", "Low_complexity", sep=" ")
-
-pieTitleVec <- c(paste("LG6hGlcNAc1s15",
-"LG6hGlcNAc2s16",
-"LG6hGlcNAc3s17",
-"HGGlcNAc1s12",
-"HGGlcNAc2s13",
-"HGGlcNAc3s14", sep = " "),
-
-paste("LG48hGlcNAc1s18",
-"LG48hGlcNAc2s19",
-"LG48hGlcNAc3s20", sep = " "),
-
-paste("LG1wGlcNAc1s21",
-"LG1wGlcNAc2s22",
-"LG1wGlcNAc3s23", sep = " "))
-
-
-outputFolder <- c("/g/boulard/Projects/O-N-acetylglucosamine/analysis/genomicDistribution/Sept2023_mouseGlucose_enhancers/",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/genomicDistribution/Sept2023_mouseGlucose_enhancers/",
-"/g/boulard/Projects/O-N-acetylglucosamine/analysis/genomicDistribution/Sept2023_mouseGlucose_enhancers/")
-
-enhancerspath <- "/g/boulard/Projects/O-N-acetylglucosamine/data/Annotations/mouse/mm10/enhancerAtlas2/E14.gff"
-
-
-
-
-# Retreives the parameters
-getParams(paramsDefinition)
+queryFileVec <- "/g/boulard/Projects/O-N-acetylglucosamine/analysis/peak_detection/macs2/Sept2023_glcPolII/mouse/glcGlucose/0.04/no_model/ESCHGGlcNAc1_lane1sample12_peaks_narrowPeak.gff"
 
 
 
