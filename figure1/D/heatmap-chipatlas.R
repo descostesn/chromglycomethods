03cc285aed95d996d44ecc2b458740ec04e00126
descoste <nicolas.descostes@embl.it>
2024-07-30 11:32:10 +0200

    unhide removeelements

--- figure1/D/heatmap-chipatlas.R
+++ figure1/D/heatmap-chipatlas.R
@@ -121,7 +121,7 @@ readandfilter <- function(resultpathvec, resultnamevec, chipatlascolnames,
     return(resultlist)
 }
 
-.removeelements <- function(resultlist, idxremove) {
+removeelements <- function(resultlist, idxremove) {
 
     if (!isTRUE(all.equal(length(idxremove), 0))) {
         message("\t Removing the following categories because of lack of ",
@@ -675,7 +675,7 @@ resultlist <- readandfilter(resultpathvec, resultnamevec, chipatlascolnames,
     ignoreqval)
 names(resultlist) <- resultnamevec
 idxremove <- which(is.na(resultlist))
-resultlist <- .removeelements(resultlist, idxremove)
+resultlist <- removeelements(resultlist, idxremove)
 nbpeaksvec <- unlist(lapply(resultlist, function(currentcat){
                     nbpeaks <- unique(as.numeric(unlist(lapply(
                         strsplit(currentcat$OverlapQuery, "/"), "[", 2))))
@@ -689,7 +689,7 @@ nbpeaksvec <- unlist(lapply(resultlist, function(currentcat){
 message("\n\n Retrieving embryonic stem cells")
 resultlistesc <- .filteroncells(resultlist)
 idxremove <- which(is.na(resultlistesc))
-resultlistesc <- .removeelements(resultlistesc, idxremove)
+resultlistesc <- removeelements(resultlistesc, idxremove)
 if (!isTRUE(all.equal(length(idxremove), 0)))
     nbpeaksvec <- nbpeaksvec[-idxremove]
 
